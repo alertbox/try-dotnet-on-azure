44c5d1db6ef6f06aa68043aae7242aa4e8b327a7
kosalanuwan <kosala.nuwan@gmail.com>
2020-11-09 05:28:52 +0000

    src: initial commit

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Api.Management.Todo
+namespace Todo.Web
 {
     public class Program
     {
--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace Api.Management.Todo
+namespace Todo.Web
 {
     public class Startup
     {
@@ -26,24 +26,25 @@ namespace Api.Management.Todo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddHealthChecks();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api.Management.Todo", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo.Web", Version = "v1" });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseHealthChecks("/health");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api.Management.Todo v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo.Web v1"));
             }
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
