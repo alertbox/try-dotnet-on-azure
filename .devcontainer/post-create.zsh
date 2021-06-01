2355b8594e24f1d696967889beaca8e406ca5cd3
kosalanuwan <kosala.nuwan@gmail.com>
2021-06-01 14:37:24 +0000

    chore: clean up sql from container

--- .devcontainer/post-create.zsh
+++ .devcontainer/post-create.zsh
@@ -3,10 +3,6 @@ echo "Trust the cert on the current platform"
 dotnet dev-certs https --clean
 dotnet dev-certs https -t
 
-echo 'Prepare SQL database.'
-# parameters: $1=SA password, $2=dacpac path, $3=sql script(s) path
-/bin/bash .devcontainer/mssql/postCreateCommand.sh 'P@ssw0rd' './bin/Debug/' './.devcontainer/mssql/'
-
 # If there's a Solution file, then run `dotnet restore`
 # It's assumed that the Solution file has all the projects added.
 if [ -f ./*.sln ]; 
